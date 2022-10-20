7c7464026feb2aa810e2b77ea0749b7cac922b07
Abhi Patel <mr.abhiofficial100@gmail.com>
2022-10-20 18:57:50 +0530

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,html}",
+    "./components/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {},
